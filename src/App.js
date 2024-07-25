b060e6432d1956501f69c0062bf966bf10136f53
thesunnymallick <alfesunnymallick800@gmail.com>
2024-07-25 12:26:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App w-full h-[100vh] flex justify-center items-center">
+    <h2 className="text-orange-500 text-xl font-semibold">Welcome PMS</h2>
+    </div>
+  );
+}
+
+export default App;
